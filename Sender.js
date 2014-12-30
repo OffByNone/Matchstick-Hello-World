4ec2eb3f1567bf555a73b0cc8240a9b13bc32c0c
CompleteAbstraction <jeremy.mcneil@gmail.com>
2014-12-29 22:54:19 -0600

    Fixed typo and Updated IP Addresses

--- Sender.js
+++ Sender.js
@@ -1,17 +1,17 @@
 "use strict";
 (function() {
     var appid = "~HelloWorld"; //Unique id of your application, must start with a ~
-    var matchstickIPAddress = "192.168.1.14"; //IP address of the matchstick
-    var receiverAppUrl = "//offbynone.github.io/matchstick-hello-world/Receiver.html"; //Url of the page to load on the receiver
+    var matchstickIPAddress = "192.168.1.232"; //IP address of the matchstick
+    var receiverAppUrl = "http://192.168.1.182:8080/matchstick-hello-world/Receiver.html"; //Url of the page to load on the receiver
     var senderDaemon = new SenderDaemon(matchstickIPAddress, appid); //comes from the sender api, is the object which will be used to communicate with the matchstick
 
     document.getElementById("launch").onclick  = function(){
         var timeout = -1; //after not communicating with the sender for this many milliseconds return to the default matchstick screen. -1 means don't timeout
         var useInterprocessCommunication = true; //not sure what this means for my application
-        senderDaemon.openApp(receiverAppUrl, timeout, userInterprocessCommunication);
+        senderDaemon.openApp(receiverAppUrl, timeout, useInterprocessCommunication);
     };
     
     document.getElementById("close").onclick = function(){
-        senderDaemon.closeApp(); 
+        senderDaemon.closeApp();
     };
 })();
